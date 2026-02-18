080ee6198cdb11cc70aab8b5126eb1386c43e8f5
Miftakun Niam <68359819+Miftakun@users.noreply.github.com>
2026-02-18 21:26:54 +0700

    Bump version to 1.0.2 and update .gitignore to exclude sensitive files.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mifdev.mathgame"
         minSdk = 21
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.0.1"
+        versionCode = 7
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -47,7 +47,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    // Removed duplicate older versions
     implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.android.gms:play-services-location:21.0.1")
